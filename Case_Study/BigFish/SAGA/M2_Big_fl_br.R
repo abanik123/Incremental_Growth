1e5616c854c568f773a8683a0fe6e666270bec6e
Arjun Banik <94594459+abanik123@users.noreply.github.com>
2025-12-05 11:38:45 -0800

    Update M2_Big_fl_br.R

--- Case_Study/BigFish/SAGA/M2_Big_fl_br.R
+++ Case_Study/BigFish/SAGA/M2_Big_fl_br.R
@@ -9,7 +9,7 @@ library(abind)
 #' Read in the data. 
 #------------------------------------------------------------------------#
 
-cap_data <- read.csv("Bigfish_capture_full.csv")
+cap_data <- read.csv("Bigfish_capture_full_wuk_sex.csv")
 dd_fl <- read.csv("Bigfish_forklength_full.csv")
 b_fl <- read.csv("Bigfish_breeding_full_wuk_sex.csv")
 
@@ -344,3 +344,4 @@ end.time <- Sys.time()
 
 time.taken <- end.time - start.time
 time.taken
+
